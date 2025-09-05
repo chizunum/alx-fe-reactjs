1fe59ec409ddfe3e119f50f4082ad3f3de42bde6
Chizunum Nlerum <Chizunum@gmail.com>
2025-09-05 06:07:48 +0100

    An updated react app

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+<footer>
+  <p>© 2023 City Lovers</p>
+</footer>
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'navy', textAlign: 'center', color: 'white'}}>
+      <p style={{ textAlign: 'center'}}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: 'navy', textAlign: 'center', color: 'white'}}>
+  <p style={{ textAlign: 'center'}}>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+  );
+}
+
+export default MainContent;
