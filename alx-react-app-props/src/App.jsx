a7b67d2a51c9ae82c1f5bef22bbcbda1c42d82a9
Chizunum Nlerum <Chizunum@gmail.com>
2025-09-06 05:55:07 +0100

    Prop drilling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,8 +7,9 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import ProfilePage from "./components/ProfilePage";
-import UserContext from "./UserContext";
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
 
 function App() {
   const [count, setCount] = useState(0);
